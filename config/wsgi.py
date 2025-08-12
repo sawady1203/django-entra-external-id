68c6c2c896a664708a621b6804aac5e2b70d26e3
sawady1203 <sawady.dev@gmail.com>
2025-08-12 13:30:54 +0900

    deploy

--- config/wsgi.py
+++ config/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+app = application  # For compatibility with some WSGI servers that expect 'app' to be the WSGI callable
