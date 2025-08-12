e405ab730a52ce85d91ead3aa6c3733ee807a62d
sawady1203 <sawady.dev@gmail.com>
2025-08-12 16:57:34 +0900

    python3.12

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,13 @@
 echo "BUILD START"
 
 # create a virtual environment named 'venv' if it doesn't already exist
-python3.9 -m venv venv
+python3.12 -m venv venv
 
 # activate the virtual environment
 source venv/bin/activate
+
+# upgrade pip to the latest version
+pip install --upgrade pip
 
 # install all deps in the venv
 pip install -r requirements.txt
